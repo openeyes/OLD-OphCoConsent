09abe703d4ad9ed438b6ee14f4b34df983935eb9
Mark <m@rkw.me>
2013-12-05 13:51:29 +0000

    [OE-3856] migration bugfixes

--- migrations/m130111_104627_element_type_eye_values.php
+++ migrations/m130111_104627_element_type_eye_values.php
@@ -6,9 +6,9 @@ class m130111_104627_element_type_eye_values extends CDbMigration
 	{
 		$event_type = EventType::model()->find('class_name=?',array('OphCoConsent'));
 		$element_type = ElementType::model()->find('event_type_id=? and class_name=?',array($event_type->id,'Element_OphCoConsent_Procedure'));
-		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>1,'display_order'=>3));
-		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>2,'display_order'=>1));
-		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>3,'display_order'=>2));
+		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>1,'display_order'=>3,'created_user_id'=>1,'last_modified_user_id'=>1));
+		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>2,'display_order'=>1,'created_user_id'=>1,'last_modified_user_id'=>1));
+		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>3,'display_order'=>2,'created_user_id'=>1,'last_modified_user_id'=>1));
 	}
 
 	public function down()
